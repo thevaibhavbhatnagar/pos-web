10fbd789632e147038fe99036cb6192226916caf
vaibhavbhatnagar-phppoets <24vaibhavbhatnagar@gmail.com>
2026-05-11 09:51:35 +0530

    refactor(dashboard): simplify dashboard page logic and remove obsolete diff script

---
+++ generate-git-commit.ps1
@@ -0,0 +1,15 @@
+# $diff = git diff
+# $prompt = "Generate a git commit message for this change:`n`n$diff"
+# $encoded = [System.Uri]::EscapeDataString($prompt)
+
+# Start-Process "https://chatgpt.com/?prompt=$encoded"
+$diff = git diff --stat
+$encoded = [System.Uri]::EscapeDataString("Generate git commit message for:`n$diff")
+
+Start-Process "https://chatgpt.com/?prompt=$encoded"
+
+# chat =>project
+# https://chatgpt.com/g/g-p-694287ede9888191badf3a9f5c175537-use-react-form-instead-of-formik/project?prompt=%22hello%22
+
+# chat =>project=>specific chat
+# https://chatgpt.com/g/g-p-694287ede9888191badf3a9f5c175537/c/6a01585f-4d0c-8323-9e4e-ebb398209248?prompt=%22update%20commit.ps1%22
